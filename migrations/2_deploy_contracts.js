256ee65fd49bb1dc72b8d70f7717262bf7d07681
“wafflemakr” <wafflemakr@gmail.com>
2019-07-12 18:13:51 -0500

    add tronbaba market contract structure

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,11 @@
 var Tronbaba = artifacts.require("./Tronbaba.sol");
+var Market = artifacts.require("./Market.sol");
 let owner = "412ce21916f8c790a66e16111dcfa0dd92e31f0efe";
 
 module.exports = function(deployer) {
-  deployer.deploy(Tronbaba, "Tronbaba", "BABA", 6, 27000000000, owner);
+  deployer
+    .deploy(Tronbaba, "Tronbaba", "BABA", 6, 27000000000, owner)
+    .then(() => {
+      return deployer.deploy(Market, Tronbaba.address);
+    });
 };
